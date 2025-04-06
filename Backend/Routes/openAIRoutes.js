58ad48d7ff55043841f6f6e34aeac7b6d1bf21f6
Yasin-shaik <skyasin2004@gmail.com>
2025-04-06 22:50:40 +0530

    Backend bug fixed

--- Backend/Routes/openAIRoutes.js
+++ Backend/Routes/openAIRoutes.js
@@ -1,10 +1,10 @@
-import {Router} from "express";
-import {generate} from "../Controllers/openAIController.js";
+import { Router } from "express";
+import { generate } from "../Controllers/openAIController.js";
 
-const router=Router();
-router.post('/generate',generate);
-router.get('/generate',(req,res)=>{
-    res.send('Hello');
-})
+const router = Router();
+router.post("/generate", generate);
+router.get("/generate", (req, res) => {
+  res.send("Hello");
+});
 
 export default router;
